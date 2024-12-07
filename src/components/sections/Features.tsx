4256f5c6d4b14bdbafe98f7c203aecb95223a92c
Fuggel <furkanserdarceylan@gmail.com>
2024-12-07 16:11:38 +0100

    style: shrink feature images on mobile

--- src/components/sections/Features.tsx
+++ src/components/sections/Features.tsx
@@ -28,8 +28,8 @@ export default function Features({ ref }: FeaturesProps) {
                                 height={350}
                                 quality={100}
                                 unoptimized
-                                className="w-[350px] md:w-[350px]"
-                                sizes="(max-width: 640px) 350px, 350px"
+                                className="w-[250px] md:w-[350px]"
+                                sizes="(max-width: 640px) 250px, 350px"
                             />
                         </Blurring>
 
@@ -52,8 +52,8 @@ export default function Features({ ref }: FeaturesProps) {
                                 height={350}
                                 quality={100}
                                 unoptimized
-                                className="w-[350px] md:w-[350px]"
-                                sizes="(max-width: 640px) 350px, 350px"
+                                className="w-[250px] md:w-[350px]"
+                                sizes="(max-width: 640px) 250px, 350px"
                             />
                         </Blurring>
 
@@ -75,8 +75,8 @@ export default function Features({ ref }: FeaturesProps) {
                                 height={350}
                                 quality={100}
                                 unoptimized
-                                className="w-[350px] md:w-[350px]"
-                                sizes="(max-width: 640px) 350px, 350px"
+                                className="w-[250px] md:w-[350px]"
+                                sizes="(max-width: 640px) 250px, 350px"
                             />
                         </Blurring>
 
